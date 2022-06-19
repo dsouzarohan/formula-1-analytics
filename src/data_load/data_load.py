eafb45c62dc4ace4e95c56c49acb1630421de7c1
Rohan Dsouza <dsouzarohan239@gmail.com>
2022-06-19 22:12:07 +0530

    Updated data_load.py to read CSV data and insert it into the required table

--- src/data_load/data_load.py
+++ src/data_load/data_load.py
@@ -1,14 +1,66 @@
 import os
 from os import listdir
 from os.path import join
+from src.migrations.database import database as db
+import csv
 
 # Dataset path, TODO: Move this to a config file so it can be changed
 path = "D:\\Documents\\Python Projects\\formula-1-analytics\\data\\external"
 
 
 def read_datasets():
-    print("Printing dataset names")
     files = os.listdir(path)
 
-    file = open(join(path, files[0]), "r")
-    print(file.read())
+    conn, err = db.get_connection()
+    if err:
+        print("Error in connecting with database", err)
+    curr = conn.cursor()
+
+    with open(join(path, files[0]), "r") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        query = """
+        INSERT INTO CIRCUITS (
+            circuitId,
+            circuitRef,
+            name,
+            location,
+            country,
+            lat,
+            lng,
+            altitude,
+            url
+        ) VALUES (
+            %(circuitId)s,
+            %(circuitRef)s,
+            %(name)s,
+            %(location)s,
+            %(country)s,
+            %(lat)s,
+            %(lng)s,
+            %(alt)s,
+            %(url)s           
+        )
+        """
+
+        for row in reader:
+            print(type(row))
+
+            data = {'circuitId': int(row['circuitId'])
+                , 'circuitRef': row['circuitRef']
+                , 'name': row['name']
+                , 'location': row['location']
+                , 'country': row['country']
+                , 'lat': row['lat']
+                , 'lng': row['lng']
+                , 'alt': None if row['alt'].find('N') > 0 else row['alt']
+                , 'url': row['url']
+            }
+
+            print(data)
+
+            curr.execute(query, data)
+
+        conn.commit()
+        curr.close()
+        conn.close()
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from data_load import data_load
 from migrations import drivers
 
-# data_load.read_datasets()
-
-drivers.up()
+data_load.read_datasets()
