a945dc80e38da30797f8a76cd9b7b4fe3bb117ff
Rohan Dsouza <dsouzarohan239@gmail.com>
2023-10-18 20:57:29 +0530

    fix(sql): query update
    added not null position

--- pgsql/india_f1/points_by_drivers.sql
+++ pgsql/india_f1/points_by_drivers.sql
@@ -5,5 +5,6 @@ FROM
     drivers d JOIN results res on d.driverid = res.driverid
               JOIN races rc on res.raceid = rc.raceid
 WHERE rc.name = 'Indian Grand Prix'
+  AND res.position IS NOT NULL
 GROUP BY d.forename
 ORDER BY total_points DESC
