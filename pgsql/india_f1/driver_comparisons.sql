1b510dc27097e8a0ee5cacd612a5316e46ff0f85
Rohan Dsouza <dsouzarohan239@gmail.com>
2024-08-24 19:41:10 +0530

    sql: added driver comparisons sql

--- pgsql/india_f1/driver_comparisons.sql
+++ pgsql/india_f1/driver_comparisons.sql
@@ -1,15 +1,25 @@
+/*
+
+ */
+
+
 SELECT d.forename,
        lt.lap lap_number,
        lt.timeinmillis,
-       lt.time
+       lt.time,
+       CASE WHEN pt.stop IS NOT NULL THEN 'stop' ELSE null END as is_pitstop,
+       rc.year
 FROM drivers d
          JOIN results res on d.driverid = res.driverid
          JOIN races rc on res.raceid = rc.raceid
          JOIN lap_times lt on rc.raceid = lt.raceid AND res.driverid = lt.driverid
+         LEFT JOIN pit_stops pt on pt.lap = lt.lap AND pt.raceid = lt.raceid AND pt.driverid = lt.driverid
 WHERE 1 = 1
---   AND rc.name = 'São Paulo Grand Prix'
-  AND rc.circuitid = 18
-  AND rc.year = 2021
+  AND rc.name = 'Indian Grand Prix'
+  AND rc.year = 2013
   AND res.position IS NOT NULL
-  AND d.refname IN ('max_verstappen', 'hamilton')
-ORDER BY d.forename, lap_number
+  AND res.position <= 5
+--   AND d.refname IN ('vettel', 'webber', 'alonso', 'hamilton')
+
+ORDER BY rc.year, d.forename, lap_number
+-- LIMIT 5
